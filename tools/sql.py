57a39bd7c3bc052185c5f866b1ad94650c925feb
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-08-20 02:31:20 +0800

    sql update... solve problem of multiple uid

--- tools/sql.py
+++ tools/sql.py
@@ -10,33 +10,42 @@ from os.path import isfile, join
 
 # 根據傳入暱稱搜尋對應 UID。
 # -1 表示傳入暱稱直接找不到對照值，可以理解爲 7/23 04:46 之後的暱稱
-# -2 表示傳入暱稱無法在多個使用者中找到對應值，也可以理解爲 7/23 04:46 之後的暱稱
+
 def searchUid(originName, cacheTime, userData, rawUserData):
     def find(name):
         if len(userData[name]) > 1:
             for uid in userData[name]:
-                index = [i for i, x in enumerate(rawUserData[uid]["name"]) if x == name]
+                index = [i for i, x in enumerate(
+                    rawUserData[uid]["name"]) if x == name]
 
+                # 針對重名
                 for i in index:
                     if int(rawUserData[uid]["time"][i]) <= int(cacheTime) and int(rawUserData[uid]["time"][i+1]) > int(cacheTime):
                         return uid
+
+                # 若還是找不到，那就再根據 timestamp
+                i = 0
+                length = len(rawUserData[uid]["time"])
+                while i < length:
+                    if i == length - 1 and int(cacheTime) > int(rawUserData[uid]["time"][i]):
+                        return uid
+                    elif int(cacheTime) >= int(rawUserData[uid]["time"][i]) and int(cacheTime) < int(rawUserData[uid]["time"][i+1]):
+                        return uid
+
+                    i += 1
+
         else:
             return userData[name][0]
 
-        
-        print(name, cacheTime)
-        return -2
-
     try:
         return find(originName)
-    except:        
+    except:
         try:
             name = [n for n in userData if originName in n][0]
             return find(name)
         except:
             print(originName)
             return -1
-
 
 
 def main():
@@ -48,11 +57,11 @@ def main():
         userData = json.load(f)
 
     for i in range(60, 86):
-        d = f"./d/{i}"
+        d = f"../d/{i}"
         files = [f for f in listdir(d) if isfile(join(d, f))]
 
         for f in files:
-            with open(f"./d/{i}/{f}", "r") as temp:
+            with open(f"../d/{i}/{f}", "r") as temp:
                 data = json.load(temp)
 
             did = f.split(".")[0]
@@ -73,7 +82,9 @@ def main():
                     floors.append(temp)
                     participant.append(uid)
 
-            conn.execute("INSERT INTO allData(did, participant, floors, cacheTime) VALUES(?,?,?,?)", (did, json.dumps(participant, ensure_ascii=False), json.dumps(floors, ensure_ascii=False), cacheTime))
+            conn.execute("INSERT INTO allData(did, participant, floors, cacheTime) VALUES(?,?,?,?)", (did, json.dumps(
+                participant, ensure_ascii=False), json.dumps(floors, ensure_ascii=False), cacheTime))
+
 
 conn = sqlite3.connect('search.sqlite3')
 print("成功連接資料庫")
